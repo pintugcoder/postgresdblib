abaede135bf7a8e5eb29be1c0112cf549099dbe7
PINTU GUPTA <pintu.gupta@ibm.com>
2023-05-11 19:30:29 +0530

    postres db lib

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client } = require("pg");
+const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "usermanagement",
+    password: "SpaccePassword@123",
+    port: 5432,
+});
+
+client.connect();
+
+module.exports = client;
